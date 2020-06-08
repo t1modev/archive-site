2596200600d45509217e422f1399277e47770370
PyTimDeveloper <PyTimDev@gmail.com>
2020-06-08 19:59:23 +0300

    First commit

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1591632172,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/scirralogo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
